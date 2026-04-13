1bf80f7ec2a541d535a578ca98389d742f76aee1
huhao0208 <123huzhifeng@163.com>
2026-04-13 19:49:22 +0800

    fix: 注册所有新组件到 VitePress 主题，修复文档预览问题

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -10,6 +10,15 @@ import HoButton from '../../../packages/components/src/button/index.vue'
 import HoInput from '../../../packages/components/src/input/index.vue'
 import HoIcon from '../../../packages/components/src/icon/index.vue'
 import HoModal from '../../../packages/components/src/modal/index.vue'
+import HoCarousel, { HoCarouselItem } from '../../../packages/components/src/carousel/index.vue'
+import HoTabs from '../../../packages/components/src/tabs/index.vue'
+import HoTab from '../../../packages/components/src/tab/index.vue'
+import HoNavBar from '../../../packages/components/src/navbar/index.vue'
+import HoTabBar from '../../../packages/components/src/tabbar/index.vue'
+import HoTabBarItem from '../../../packages/components/src/tabbar-item/index.vue'
+import HoImage from '../../../packages/components/src/image/index.vue'
+import HoCell from '../../../packages/components/src/cell/index.vue'
+import HoCellGroup from '../../../packages/components/src/cell-group/index.vue'
 import { message, toast } from '../../../packages/components/src/index'
 
 export default {
@@ -23,6 +32,16 @@ export default {
     app.component('HoInput', HoInput)
     app.component('HoIcon', HoIcon)
     app.component('HoModal', HoModal)
+    app.component('HoCarousel', HoCarousel)
+    app.component('HoCarouselItem', HoCarouselItem)
+    app.component('HoTabs', HoTabs)
+    app.component('HoTab', HoTab)
+    app.component('HoNavBar', HoNavBar)
+    app.component('HoTabBar', HoTabBar)
+    app.component('HoTabBarItem', HoTabBarItem)
+    app.component('HoImage', HoImage)
+    app.component('HoCell', HoCell)
+    app.component('HoCellGroup', HoCellGroup)
     
     // 注册全局 API
     app.config.globalProperties.$message = message
